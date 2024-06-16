94042b0754c2c47d02b608f8d98cd4a1a4c93b75
Семён Замолоцкий <>
2024-06-17 02:39:08 +0700

    SEO optimization

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import './style.css';
 import {App} from './app/app';
 
 if ('serviceWorker' in navigator) {
-  // navigator.serviceWorker
-  //   .register('./sw.js')
-  //   .catch(console.error);
+  navigator.serviceWorker
+    .register('./sw.js')
+    .catch(console.error);
 }
 
 const app = new App();
